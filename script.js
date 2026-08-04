411f46d4700914acd5cd113680f06cd596f0284f
Ransom <ransomart@protonmail.com>
2026-08-03 23:05:40 -0500

    Content Update
    Updated top-nav and pushed some early content.

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+PAGE_ROOT = "pages/"
+
+document.querySelectorAll(".nav-link").forEach(link => {
+	link.addEventListener("click", event => {
+		event.preventDefault();
+		
+		if (link.classList.contains("disabled")) {
+			return;
+		}
+		
+		loadPage(link.dataset.page, "#content");
+	});	
+});
+
+function setActivePage(pageName) {
+	
+	const links = document.querySelectorAll(".nav-link");
+	
+	links.forEach(element => {
+		element.classList.toggle("active", element.dataset.page === pageName);
+	});	
+}
+
 async function loadPage(page, targetID)
 {
-	const response = await fetch(page);
+	const response = await fetch(`${PAGE_ROOT}${page}.html`);
+	
 	
 	const html = await response.text();
+	document.querySelector(targetID).innerHTML = html;
+	setActivePage(page)
 	
-	document.getElementById(targetID).innerHTML = html;
 }
 
 window.onload = function()
 {
-	loadPage("pages/main.html", "content");
-	loadPage("pages/sidebar.html", "sidebar")
+	loadPage("sidebar", "#sidebar");
+	loadPage("main", "#content");
 }
